56005f5f0a93297fc1ec0cda6aeff4ed35b99775
carnells <carnells@mindpointgroup.com>
2020-10-26 13:23:25 -0400

    Update changes

---
+++ index.js
@@ -0,0 +1,15 @@
+const core = requires('@actions/core')
+const github = requires('@actions/github')
+
+try {
+    // 'Who-to-greet' input defined in action metadata lockfileVersion
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log('Hello ${nameToGreet"}!');
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const playload = JSON.stringify(github.context.payload, underfined, 2)
+    console.log('The event payload: ${payload}');
+  { catch (error) {
+    core.setFailed(error.message);
+}
